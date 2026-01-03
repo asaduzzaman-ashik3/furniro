94d0db2b036f667975de346b921ff3d7aa87eebd
Assaduzzaman Ashik <ashikhossain372@gmail.com>
2026-01-03 16:45:34 +0600

    browse the range section add

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
-import Image from "next/image";
-import BrowseSection from './../components/landingPage/sections/browseSection';
+import BrowseSection from "@/components/landingPage/sections/browseSection";
 
 export default function Home() {
   return (
-    <main>
-      <section
-        className="h-[80vh] bg-cover bg-center bg-no-repeat relative"
-        style={{ backgroundImage: `url('/images/hego-img.jpg')` }}
-      >
-        <div className="bg-[#FFF3E3] p-10 absolute bottom-[20%] right-20 w-160 space-y-4">
-          <p className="text-md">New Arrival</p>
-          <h1 className="text-5xl font-bold text-[#B88E2F]">Discover Our <br />New Collection</h1>
-          <p className="text-lg font-medium">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-          <button className="bg-[#B88E2F] text-white px-12 py-4 text-sm font-bold lg:mt-6">Shop Now</button>
-        </div>
-      </section>
-      <BrowseSection></BrowseSection>
-    </main>
+    <div>
+      <main>
+        <section
+          className="h-[80vh] bg-cover bg-center bg-no-repeat relative"
+          style={{ backgroundImage: `url('/images/hego-img.jpg')` }}
+        >
+          <div className="bg-[#FFF3E3] p-10 absolute bottom-[20%] right-20 w-160 space-y-4">
+            <p className="text-lg">New Arrival</p>
+            <h1 className="text-5xl font-bold text-[#B88E2F]">Discover Our <br />New Collection</h1>
+            <p className="text-lg font-medium">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+            <button className="bg-[#B88E2F] text-white px-12 py-4 text-sm">Shop Now</button>
+          </div>
+        </section>
+      </main>
+      <BrowseSection />
+    </div>
   );
 }
+
--- components/landingPage/sections/browseSection.tsx
+++ components/landingPage/sections/browseSection.tsx
@@ -17,36 +17,30 @@ export default function BrowseSection() {
     ];
 
     return (
-        <section className="py-16 px-6 bg-gray-100">
-            {/* Section title */}
-            <h2 className="text-4xl font-bold text-center mb-6">
-                Browse Our Collection
+        <section className="py-16 px-6 h-screen w-13/15 mx-auto">
+            <h2 className="text-3xl font-bold text-center mb-1">
+                Browse The Range
             </h2>
 
-            {/* Section description */}
-            <p className="text-center text-gray-600 mb-12">
-                Discover amazing products handpicked just for you.
+            <p className="text-center text-lg text-[#666666] mb-12">
+                 Discover amazing products handpicked just for you, crafted to inspire and delight every day.
             </p>
 
-            {/* Grid of cards */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 h-[90%]">
                 {cards.map((card, index) => (
                     <div
                         key={index}
-                        className="bg-white rounded-xl shadow-lg overflow-hidden flex flex-col items-center text-center"
+                        className="bg-white overflow-hidden flex flex-col items-center text-center"
                     >
-                        {/* Card image */}
-                        <div className="w-full h-56 relative">
+                        <div className="w-full h-full relative">
                             <Image
                                 src={card.image}
                                 alt={card.title}
                                 fill
-                                className="object-cover"
+                                className="object-cover rounded-2xl"
                             />
                         </div>
-
-                        {/* Card title */}
-                        <h3 className="text-xl font-semibold my-4">{card.title}</h3>
+                        <h3 className="text-xl font-semibold mt-7">{card.title}</h3>
                     </div>
                 ))}
             </div>
