b0d175883ba45e131a1d35225190aaba4e8106b0
Assaduzzaman Ashik <ashikhossain372@gmail.com>
2026-01-03 16:17:51 +0600

    landing hero section add

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,11 @@ export default function Home() {
         className="h-[80vh] bg-cover bg-center bg-no-repeat relative"
         style={{ backgroundImage: `url('/images/hego-img.jpg')` }}
       >
-        <div className="bg-[#FFF3E3] p-10 absolute top-[50%] right-20">
-          <p className="">New Arrival</p>
-          <h1 className="text-5xl font-bold">Welcome</h1>
-          <p>Discover your dream property</p>
+        <div className="bg-[#FFF3E3] p-10 absolute bottom-[20%] right-20 w-160 space-y-4">
+          <p className="text-md">New Arrival</p>
+          <h1 className="text-5xl font-bold text-[#B88E2F]">Discover Our <br />New Collection</h1>
+          <p className="text-lg font-medium">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+          <button className="bg-[#B88E2F] text-white px-12 py-4 text-sm font-bold lg:mt-6">Shop Now</button>
         </div>
       </section>
     </main>
